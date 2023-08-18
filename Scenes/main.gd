feb380b31122ae76aaecc723d212e4b73e60fd16
PudWin14 <petomarfo@gmail.com>
2023-08-18 20:37:02 +0200

    Added: Resizing of grid container
    The Cells Grid container resize in function
    of number of cells. Keep the size of cells

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,6 +3,8 @@ extends Node
 var CellScene = preload("res://Scenes/cell.tscn")
 var status_grid = []
 
+@onready var CellsGrid = get_node("CellsGrid")
+
 signal click_cell_main(pos : Vector2)
 
 func _ready():
@@ -12,7 +14,6 @@ func _ready():
 	status_grid = new_status_grid()
 	$HUD.new_game_pressed.connect(restart_game)
 	$HUD.update_hud()
-
 
 
 func end_game():
@@ -56,7 +57,9 @@ func win_check():
 
 func display_cells(num_rows,num_cols):
 	
-	$CellsGrid.columns = num_cols
+	CellsGrid.columns = num_cols
+	CellsGrid.size = Vector2(24*num_cols,24*num_rows)
+	
 	for row in num_rows:
 		for col in num_cols:
 			var new_cell = CellScene.instantiate()
@@ -69,20 +72,21 @@ func display_cells(num_rows,num_cols):
 			new_cell.click_around.connect(click_around_cells)
 			click_cell_main.connect(new_cell.click_cell_from_main)
 			
-			$CellsGrid.add_child(new_cell)
+			CellsGrid.add_child(new_cell)
+
 
 
 func delate_cells():
-	for child in $CellsGrid.get_children():
+	for child in CellsGrid.get_children():
 		child.queue_free()
 
 func new_game():
 	GlobalVariables.new_game()
 	status_grid = new_status_grid()
-	$CellsGrid.hide()
+	CellsGrid.hide()
 	delate_cells()
 	display_cells(GlobalVariables.MAX_ROWS,GlobalVariables.MAX_COLS)
-	$CellsGrid.show()
+	CellsGrid.show()
 
 func new_status_grid():
 	var new_st_grid = []
