46fa9674684c43c464ebb11f1d97ba7c558be231
PudWin14 <petomarfo@gmail.com>
2023-09-21 18:34:58 +0200

    Updated default size on custom level

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -28,9 +28,9 @@ func _on_hard_button_pressed():
 
 
 func _on_custom_button_pressed():
-	GlobalVariables.new_max_cols = 9
-	GlobalVariables.new_max_rows = 9
-	GlobalVariables.new_num_mines = 10
+	GlobalVariables.new_max_cols = 16
+	GlobalVariables.new_max_rows = 16
+	GlobalVariables.new_num_mines = 40
 	GlobalVariables.new_game()
 	GlobalVariables.custom = true
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
